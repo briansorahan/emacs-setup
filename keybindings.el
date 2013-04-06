9a475c2b81e40df969166f6621c9c0e9b2ce6942
sorahan <sorahan@sorahan-desktop.(none)>
2013-04-06 16:19:44 -0500

    added *.el files to modes. added coffeescript. took out fit-frame and zencoding

--- keybindings.el
+++ keybindings.el
@@ -1,14 +1,13 @@
-;;
 ;; Key-bindings
-;;
+
 ;; buffer-move
 (global-set-key (kbd "<C-S-up>") 'buf-move-up)
 (global-set-key (kbd "<C-S-down>") 'buf-move-down)
 (global-set-key (kbd "<C-S-left>") 'buf-move-left)
 (global-set-key (kbd "<C-S-right>") 'buf-move-right)
-;;
+
 ;; my favs
 (global-set-key (kbd "C-<DEL>") 'backward-kill-word)
 (global-set-key (kbd "C-<SPC>") 'forward-word)
 (global-set-key (kbd "C-o") 'scroll-down)
-;;
+
