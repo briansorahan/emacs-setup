baa9803fe94dccb565cce04bb06dba66f46c70c3
brian sorahan <bsorahan@gmail.com>
2013-05-08 10:32:10 -0500

    took out workgroups

--- keybindings.el
+++ keybindings.el
@@ -1,13 +1,9 @@
 ;; Key-bindings
 
 ;; buffer-move
-(global-set-key (kbd "<C-S-up>") 'buf-move-up)
-(global-set-key (kbd "<C-S-down>") 'buf-move-down)
 (global-set-key (kbd "<C-S-left>") 'buf-move-left)
 (global-set-key (kbd "<C-S-right>") 'buf-move-right)
 
-;; my favs
-(global-set-key (kbd "C-<DEL>") 'backward-kill-word)
-(global-set-key (kbd "C-<SPC>") 'forward-word)
+;; navigation
 (global-set-key (kbd "C-o") 'scroll-down)
 
